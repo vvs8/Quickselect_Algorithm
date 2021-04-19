fbbe0d443413b168ec3a2e84f9d628dc4098e784
Vadym Shakhraichuk <64581387+vvs8@users.noreply.github.com>
2021-04-18 18:24:56 -0700

    Update QuickSelect.java

--- QuickSelect.java
+++ QuickSelect.java
@@ -2,41 +2,41 @@ import java.util.*;
 
 public class QuickSelect {
 
-  public int findKthSmallest(int[] A, int k) {
-	  int result_index = quickSelect(A, 0, A.length-1, A.length-k);
-	  return A[result_index];
-  }
-
-  public int quickSelect(int A[], int low, int high, int k) {
-	  int partition_index = partition(A, low, high);
-	  if(partition_index == k) 
-      return partition_index;
-	  else if (partition_index > k) 
-      return quickSelect(A, low, partition_index-1, k);
-	  else 
-      return quickSelect(A, partition_index+1, high, k);
-  }
-
-  public int partition(int A[], int low, int high) {
-	  int random_index = (int) (low + (Math.random()*(high-low+1)));
-	  swap(A, high, random_index);
-	  int pivot = A[high];
-	  int index = low;
-	  for(int i = low ; i < high ; i++){
-	  	if(A[i] >= pivot){
-		  	swap(A, i, index);
-		  	index++;
-	  	}
+	public int findKthSmallest(int[] A, int k) {
+		int result_index = quickSelect(A, 0, A.length-1, A.length-k);
+	  	return A[result_index];
   	}
-	  swap(A, index, high);
-	  return index;
-  }
 
-  public void swap(int A[], int i , int j){
-	  int temp = A[i];
-	  A[i] = A[j];
-	  A[j] = temp;
-  }
+	public int quickSelect(int A[], int low, int high, int k) {
+		int partition_index = partition(A, low, high);
+		if(partition_index == k) 
+      			return partition_index;
+	  	else if (partition_index > k) 
+      			return quickSelect(A, low, partition_index-1, k);
+	  	else 
+      			return quickSelect(A, partition_index+1, high, k);
+  	}
+
+  	public int partition(int A[], int low, int high) {
+		int random_index = (int) (low + (Math.random()*(high-low+1)));
+	  	swap(A, high, random_index);
+		int pivot = A[high];
+	  	int index = low;
+	  	for(int i = low ; i < high ; i++){
+	  		if(A[i] >= pivot){
+		  		swap(A, i, index);
+		  		index++;
+	  		}
+  		}
+	 	swap(A, index, high);
+	  	return index;
+  	}
+
+  	public void swap(int A[], int i , int j) {
+		int temp = A[i];
+	  	A[i] = A[j];
+	  	A[j] = temp;
+  	}
 
 
 
